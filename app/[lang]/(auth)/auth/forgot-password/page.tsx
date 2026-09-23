9dc4d674ab79ee3c4e3131065f53bf103f6007f1
Auez <kemelovauez4@gmail.com>
2026-09-23 23:42:29 +0500

    add  page forgot-password and register

---
+++ app/[lang]/(auth)/auth/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+export default function forgotPassword() {
+  return (
+    <div className="flex min-h-screen items-center justify-center px-4 py-12">
+      Forgot Password
+    </div>
+  );
+}
---
+++ app/[lang]/(auth)/auth/register/page.tsx
@@ -0,0 +1,7 @@
+export default function register() {
+  return (
+    <div className="flex min-h-screen items-center justify-center px-4 py-12">
+      Register
+    </div>
+  );
+}
